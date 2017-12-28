a7ce4eb8691b0298fd46a97e6116fb865239823b
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2017-12-28 18:28:09 +0800

    赶在下班前写了个bug

--- app/src/main/java/com/lx/kotlin/reader/adapter/ZhiHuThemeAdapter.kt
+++ app/src/main/java/com/lx/kotlin/reader/adapter/ZhiHuThemeAdapter.kt
@@ -1,17 +1,33 @@
 package com.lx.kotlin.reader.adapter
 
 import android.content.Context
+import com.lx.kotlin.reader.R
 import com.lx.kotlin.reader.model.bean.ThemeList
 import com.zhy.adapter.recyclerview.MultiItemTypeAdapter
+import com.zhy.adapter.recyclerview.base.ItemViewDelegate
 import com.zhy.adapter.recyclerview.base.ViewHolder
 
 /**
  * Created on 17-12-28 下午4:50
  */
-class ZhiHuThemeAdapter(context: Context?, datas: MutableList<ThemeList.OthersInfo>?) : MultiItemTypeAdapter<ThemeList.OthersInfo>(context, datas) {
+class ZhiHuThemeAdapter(context: Context, datas: MutableList<ThemeList.OthersInfo>?) : MultiItemTypeAdapter<ThemeList.OthersInfo>(context, datas) {
 
-    override fun convert(holder: ViewHolder?, t: ThemeList.OthersInfo?) {
-        super.convert(holder, t)
+    init {
+        addItemViewDelegate(ThemeItem(context))
+    }
 
+    private class ThemeItem(context: Context) :ItemViewDelegate<ThemeList.OthersInfo>{
+
+        override fun convert(holder: ViewHolder?, t: ThemeList.OthersInfo?, position: Int) {
+
+        }
+
+        override fun isForViewType(item: ThemeList.OthersInfo?, position: Int): Boolean {
+            return true
+        }
+
+        override fun getItemViewLayoutId(): Int {
+            return R.layout.item_zhihu_theme
+        }
     }
 }
