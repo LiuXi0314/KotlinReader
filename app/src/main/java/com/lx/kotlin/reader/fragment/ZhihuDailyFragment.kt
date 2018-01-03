fd5a7bb052ae86e4bebf3da3472c28f7606901ea
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2018-01-03 22:38:59 +0800

    do some work

--- app/src/main/java/com/lx/kotlin/reader/fragment/ZhihuDailyFragment.kt
+++ app/src/main/java/com/lx/kotlin/reader/fragment/ZhihuDailyFragment.kt
@@ -1,56 +1,46 @@
 package com.lx.kotlin.reader.fragment
 
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.view.View
-import com.lx.kotlin.reader.model.bean.ThemeList
-import com.lx.kotlin.reader.model.service.ServiceFactory
-import com.lx.kotlin.reader.utils.Logger
+import com.lx.kotlin.reader.model.bean.StoriesInfo
 import com.zhy.adapter.recyclerview.MultiItemTypeAdapter
-import kotlinx.android.synthetic.main.fragment_recycler.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 /**
+ *
+ * 知乎日报列表
  * Created on 17-12-4 下午2:42
  */
-class ZhihuDailyFragment{
-//    override fun loadData() {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//
-//    override fun createAdapter(): ? {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
+class ZhihuDailyFragment: RecyclerFragment<StoriesInfo>(){
+    override fun createAdapter(): MultiItemTypeAdapter<StoriesInfo> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
-//    override fun createAdapter(): MultiItemTypeAdapter<Any>? {
-////        return SlimAdapter.create().register(R.layout.item_zhihu_theme, ZhihuThemeInjector(context))
-//        return null
-//    }
-//
-//    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        loadData()
-//    }
-//
-//    override fun loadData() {
-//
-//        var api = ServiceFactory.getZhihuService()
-//        api.getZhihuTheme().enqueue(object : Callback<ThemeList> {
-//            override fun onFailure(call: Call<ThemeList>?, t: Throwable?) {
-//                Logger.log("failure")
-//                Logger.log(t.toString())
-//                swipeRefresh.isRefreshing = false
-//            }
-//            override fun onResponse(call: Call<ThemeList>?, response: Response<ThemeList>?) {
-//                swipeRefresh.isRefreshing = false
-//                Logger.log("success" + response!!.body()!!.toString())
-////                mAdapter!!.updateData(response.body()!!.others)
-//                Logger.log("success")
-//
-//            }
-//        })
-//    }
+    override fun loadData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
+    override fun createLayoutManager(): RecyclerView.LayoutManager? {
+        return super.createLayoutManager()
+    }
+
+    override fun loadMore() {
+        super.loadMore()
+    }
+
+    override fun createLoadMoreViewId(): Int {
+        return super.createLoadMoreViewId()
+    }
+
+    override fun onClick(v: View?) {
+        super.onClick(v)
+    }
+
+    override fun onItemClick(view: View?, holder: RecyclerView.ViewHolder?, position: Int) {
+        super.onItemClick(view, holder, position)
+    }
 }
