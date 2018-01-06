ac92c3159779a970104427a66f40c832b94c8ab6
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2018-01-06 18:01:01 +0800

    modify ItemTopDecoration

--- app/src/main/java/com/lx/kotlin/reader/adapter/help/ItemTopDecoration.kt
+++ app/src/main/java/com/lx/kotlin/reader/adapter/help/ItemTopDecoration.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.lx.kotlin.reader.R
@@ -19,18 +20,18 @@ import com.lx.kotlin.reader.utils.Logger
  */
 class ItemTopDecoration(val context: Context, val data: MutableList<ItemDecData>) : RecyclerView.ItemDecoration() {
 
-    private var topHeight:Float = 0F
+    private var topHeight: Float = 0F
     private val spacing = Formatter.dip2px(context, 4F)
-    var text:String = ""
 
     init {
         val paint = Paint()
         paint.setTextSize(Formatter.sp2px(context, 14.toFloat()).toFloat())
         paint.getTextBounds("test", 0, 4, Rect())
         val fontMetrics = paint.getFontMetricsInt()
-        var absHeight = Math.abs(fontMetrics.top-fontMetrics.bottom)
-        topHeight = (absHeight+30).toFloat()
+        var absHeight = Math.abs(fontMetrics.top - fontMetrics.bottom)
+        topHeight = (absHeight + 20).toFloat()
     }
+
     override fun onDraw(c: Canvas?, parent: RecyclerView?, state: RecyclerView.State?) {
         super.onDraw(c, parent, state)
         // 得到item真实的left和right（减去parent的padding）
@@ -38,54 +39,16 @@ class ItemTopDecoration(val context: Context, val data: MutableList<ItemDecData>
         val right = (parent.getWidth() - parent.getPaddingRight()).toFloat()
 
         for (i in 0 until parent.getChildCount()) {
+
             // 直接获得的child只有当前显示的，所以就算i是0的index也只是当前第一个，而不是所有第一个
             val child = parent.getChildAt(i)
             val index = parent.getChildAdapterPosition(child)
-            if (checkInGroupFirst(index)) {
-                // 每组第一个item都留有空间来绘制
-                val top = child.top - topHeight-spacing
-                val bottom = child.top.toFloat()-spacing
-                // 绘制背景色
-                val paint = Paint()
-                paint.setColor(ContextCompat.getColor(context, R.color.colorRefresh2))
-
-                c!!.drawRect(left, top, right, bottom, paint)
-                // 绘制组名
-                paint.setColor(ContextCompat.getColor(context, R.color.colorWhite))
-                paint.setTextSize(Formatter.sp2px(context, 14.toFloat()).toFloat())
-                paint.setTextAlign(Paint.Align.LEFT)
-                paint.setAntiAlias(true)
-                var str = getText(index)
-                paint.getTextBounds(str, 0, str.length, Rect())
-                val fontMetrics = paint.getFontMetricsInt()
-                val baseline =(bottom +top-fontMetrics.bottom-fontMetrics.top)/2
-                c.drawText(getText(index), left + 20, baseline, paint)
+            if (!checkInGroupFirst(index)) {
+                continue
             }
-        }
-    }
-
-
-    override fun onDrawOver(c: Canvas?, parent: RecyclerView?, state: RecyclerView.State?) {
-        super.onDrawOver(c, parent, state)
-
-        // 得到item真实的left和right（减去parent的padding）
-        val left = parent!!.getPaddingLeft().toFloat()
-        val right = (parent.getWidth() - parent.getPaddingRight()).toFloat()
-
-        for (i in 0 until parent.getChildCount()) {
-
-            // 直接获得的child只有当前显示的，所以就算i是0的index也只是当前第一个，而不是所有第一个
-            val child = parent.getChildAt(i)
-            val index = parent.getChildAdapterPosition(child)
-
             // 每组第一个item都留有空间来绘制
-            val top = parent.paddingTop.toFloat()
-            var bottom = 0F
-            if (checkInGroupFirst(index+1)){
-                bottom = Math.min(topHeight+top,child.top.toFloat())
-            }else{
-                bottom = topHeight+top
-            }
+            val top = child.top - topHeight - spacing
+            val bottom = child.top.toFloat() - spacing
             // 绘制背景色
             val paint = Paint()
             paint.setColor(ContextCompat.getColor(context, R.color.colorRefresh2))
@@ -93,16 +56,53 @@ class ItemTopDecoration(val context: Context, val data: MutableList<ItemDecData>
             c!!.drawRect(left, top, right, bottom, paint)
             // 绘制组名
             paint.setColor(ContextCompat.getColor(context, R.color.colorWhite))
-            paint.setTextSize(Formatter.sp2px(context, 14.toFloat()).toFloat())
+            paint.setTextSize(Formatter.sp2px(context, 12.toFloat()).toFloat())
             paint.setTextAlign(Paint.Align.LEFT)
             paint.setAntiAlias(true)
-            paint.getTextBounds(text, 0, text.length, Rect())
+            var str = getText(index)
+            paint.getTextBounds(str, 0, str.length, Rect())
             val fontMetrics = paint.getFontMetricsInt()
             val baseline = (bottom + top - fontMetrics.bottom - fontMetrics.top) / 2
             c.drawText(getText(index), left + 20, baseline, paint)
-
-
         }
+    }
+
+
+    override fun onDrawOver(c: Canvas?, parent: RecyclerView?, state: RecyclerView.State?) {
+        super.onDrawOver(c, parent, state)
+        // 得到item真实的left和right（减去parent的padding）
+        val left = parent!!.getPaddingLeft().toFloat()
+        val right = (parent.getWidth() - parent.getPaddingRight()).toFloat()
+
+        var index  = (parent.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+        val child = parent.getChildAt(0)
+
+        // 每组第一个item都留有空间来绘制
+        var top = 0F
+        var bottom = 0F
+        if (checkInGroupFirst(index+1)) {
+            bottom = Math.min(topHeight + parent.paddingTop, child.bottom.toFloat() +spacing)
+        } else {
+            bottom = topHeight + parent.paddingTop
+        }
+        top = bottom - topHeight
+
+        // 绘制背景色
+        val paint = Paint()
+        paint.setColor(ContextCompat.getColor(context, R.color.colorRefresh2))
+
+        c!!.drawRect(left, top, right, bottom, paint)
+        // 绘制组名
+        paint.setColor(ContextCompat.getColor(context, R.color.colorWhite))
+        paint.setTextSize(Formatter.sp2px(context, 12.toFloat()).toFloat())
+        paint.setTextAlign(Paint.Align.LEFT)
+        paint.setAntiAlias(true)
+        var text = getText(index)
+        Logger.log(text)
+        paint.getTextBounds(text, 0, text.length, Rect())
+        val fontMetrics = paint.getFontMetricsInt()
+        val baseline = (bottom + top - fontMetrics.bottom - fontMetrics.top) / 2
+        c.drawText(text, left + 20, baseline, paint)
 
     }
 
@@ -116,8 +116,7 @@ class ItemTopDecoration(val context: Context, val data: MutableList<ItemDecData>
         outRect!!.right = spacing
         outRect.bottom = spacing
         if (checkInGroupFirst(pos)) {
-            text = getText(pos)
-            outRect!!.top = topHeight.toInt()+spacing
+            outRect!!.top = topHeight.toInt() + spacing
         }
     }
 
@@ -129,9 +128,24 @@ class ItemTopDecoration(val context: Context, val data: MutableList<ItemDecData>
     }
 
     private fun getText(pos: Int): String {
-        for (item in data) {
-            if (pos == item.pos) return item.text
+        var id = getFirstId(pos)
+        for (item in data){
+            if (id == item.pos)return item.text
         }
         return ""
+    }
+
+    private fun getFirstId(pos: Int): Int {
+        var lastData: ItemDecData? = null
+        for (item in data) {
+            if (pos == item.pos)return item.pos
+
+            if (pos < item.pos) {
+                return lastData!!.pos
+            }
+
+            lastData = item
+        }
+        return 0
     }
 }
